d9fd24c2751ed90c4bc35781f2f58d486d41a488
GowriMungath <73058923+GowriMungath@users.noreply.github.com>
2021-08-29 20:05:49 +0530

    Update Python_Day1_Exercise code.py

--- Python_Day1_Exercise code.py
+++ Python_Day1_Exercise code.py
@@ -1,15 +1,35 @@
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
+
 print("Hello\nGowri\nWelcome")
 x="Gowri"+"Mungath"
 print(x)
 print("Midhun"+" "+"Mungath")
+
 print("Day 1 - String Manipulation")
 print('String Concatenation is done with the "+" sign.')
 print('e.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and n.")
+
 #Let's learn about input function
 a=input("what is your name?")
 print("Your name is"+a)
 print("You are"+input("what is your age?")+" years old.")
+
+print(len(input("Enter your name: ")))
+
+name="Gowri"
+print(name)
+name="Sreekutty"
+print(name)
+
+a = input("a: ")
+b = input("b: ")
+temp=a
+a=b
+b=temp
+print("a: " + a)
+print("b: " + b)
+
+
