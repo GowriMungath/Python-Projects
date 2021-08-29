e25288355c5cf85f38feb3492ead787c6cc2fda1
GowriMungath <73058923+GowriMungath@users.noreply.github.com>
2021-08-29 19:41:04 +0530

    Update Python_Day1_Exercise code.py

--- Python_Day1_Exercise code.py
+++ Python_Day1_Exercise code.py
@@ -9,6 +9,7 @@ print("Day 1 - String Manipulation")
 print('String Concatenation is done with the "+" sign.')
 print('e.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and n.")
+#Let's learn about input function
 a=input("what is your name?")
 print("Your name is"+a)
 print("You are"+input("what is your age?")+" years old.")
