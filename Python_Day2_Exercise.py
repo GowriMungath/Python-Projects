227d80a88c7a9c01ade0daee5033c481cd23ea11
GowriMungath <73058923+GowriMungath@users.noreply.github.com>
2021-08-30 19:23:49 +0530

    Create Python_Day2_Exercise.py

---
+++ Python_Day2_Exercise.py
@@ -0,0 +1,24 @@
+#datatypes
+print("Gowri"[0])
+print("Gowri"[3])
+print("1425"[2])
+print("123"+"456")
+print(123+456)
+print(123_456_789)
+print(3.134)
+print(True)
+print(False)
+
+city=input("Which is your favourite city? ")
+val=len(city)
+print(type(val))
+value_in_string=str(val)
+print(type(value_in_string))
+print("Your favourite city has "+value_in_string+" characters")
+
+a=123
+print(type(a))
+print(type(str(a)))
+print(type(float(a)))
+print(70+float("100.5"))
+print(str(70)+str(100))
