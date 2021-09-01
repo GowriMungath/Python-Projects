5952a5169ea7ad0271d42359f1670221ad1f9709
GowriMungath <73058923+GowriMungath@users.noreply.github.com>
2021-09-01 18:43:49 +0530

    Create Python_Day3_Exercise code.py

---
+++ Python_Day3_Exercise code.py
@@ -0,0 +1,48 @@
+number = int(input("Which number do you want to check? "))
+if number%2==0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
+
+#roller_coaster
+height=float(input("Enter your height in Cms: "))
+if height>120:
+    print("welcome to the roller coaster ride!")
+    age=int(input("Enter your age: "))
+    if age<12:
+        print("Please pay $5.")
+    elif age<=18:
+        print("Please pay $7")
+    else:
+        print("Please pay $12")
+else:
+    print("Entry denied!")
+
+#BMI_Calculator_2.0
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+bmi=round(weight/(height**2),2)
+if bmi<18.5:
+  print(f"Your BMI is {bmi} and you are underweight")
+elif bmi<25:
+  print(f"Your BMI is {bmi} and you have a normal weight")
+elif bmi<30:
+  print(f"Your BMI is {bmi} and you are slightly overweight")
+elif bmi<35:
+  print(f"Your BMI is {bmi} and you are obese")
+else:
+  print(f"Your BMI is {bmi} and you are clinically obese")
+
+#Leap_year_checker
+year = int(input("Which year do you want to check? "))
+if year%400==0:
+  print("Leap Year")
+elif year%4==0:
+  if year%100==0:
+    print("Not a leap year")
+  else:
+    print("Leap Year")
+else:
+  print("Not a leap year")
+
+
