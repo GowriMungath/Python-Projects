cd7d510c8166d99015daab225e307f4d7741d8c5
GowriMungath <73058923+GowriMungath@users.noreply.github.com>
2021-09-08 19:55:36 +0530

    Update Python_Day5_Exercise code.py

--- Python_Day5_Exercise code.py
+++ Python_Day5_Exercise code.py
@@ -10,3 +10,16 @@ for i in student_heights:
 avg=round(s/count)
 print(avg)
 
+#high score calculate
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+high=student_scores[0]
+for i in student_scores:
+  if i>high:
+    high=i
+print(f"The highest score in the class is: {high}")
+
+
+
