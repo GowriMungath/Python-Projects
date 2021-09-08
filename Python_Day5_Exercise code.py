7ae881c37e00b9c340949b8d0acd1324d0eb7dfe
GowriMungath <73058923+GowriMungath@users.noreply.github.com>
2021-09-08 18:42:55 +0530

    Update Python_Day5_Exercise code.py

--- Python_Day5_Exercise code.py
+++ Python_Day5_Exercise code.py
@@ -1,1 +1,12 @@
+#average height calculate
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+s=0
+count=0
+for i in student_heights:
+  s=s+i
+  count+=1
+avg=round(s/count)
+print(avg)
 
