e8f5640f639d4e1bb30cecee85c5ea9ec7123a52
GowriMungath <73058923+GowriMungath@users.noreply.github.com>
2021-09-11 16:52:40 +0530

    Update Python_Day6_Exercise code.py

--- Python_Day6_Exercise code.py
+++ Python_Day6_Exercise code.py
@@ -2,6 +2,7 @@ def fun():
     print("This is a function.")
 fun()
 
+#hurdle 3
 def turn_right():
      turn_left();
      turn_left();
@@ -19,6 +20,35 @@ while(at_goal()!=True):
         jump();
     else:
         move();
+
+#hurdle 4
+def turn_right():
+     turn_left();
+     turn_left();
+     turn_left();
+def check():
+     while(wall_on_right()==True):
+        if not wall_in_front():
+            move();
+        else:
+            turn_left();
+def jump():
+    turn_left();
+    check();
+    turn_right();
+    move();
+    turn_right();
+    check();
+while(at_goal()!=True):
+    if wall_in_front()==True:
+        jump();
+    else:
+        move();
+   
+
+    
+    
+   
    
     
    
