15bed08e40759ca208822ef32f44cd4322a908be
GowriMungath <73058923+GowriMungath@users.noreply.github.com>
2021-09-13 17:36:21 +0530

    Update Python_Day7_Exercise code.py

--- Python_Day7_Exercise code.py
+++ Python_Day7_Exercise code.py
@@ -9,3 +9,17 @@ for i in chosen_word:
     print("right")
   else:
     print("wrong")
+    
+#replacing blanks
+import random
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+print(f'Pssst, the solution is {chosen_word}.')
+display=[]
+for i in chosen_word:
+  display.append("_")
+guess = input("Guess a letter: ").lower()
+for i in range(len(chosen_word)-1):
+    if chosen_word[i] == guess:
+        display[i]=guess;
+print(display)
