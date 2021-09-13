de4328c3b77a2f21e71d8a5ff666f79600dbc3b1
GowriMungath <73058923+GowriMungath@users.noreply.github.com>
2021-09-13 17:57:03 +0530

    Update Python_Day7_Exercise code.py

--- Python_Day7_Exercise code.py
+++ Python_Day7_Exercise code.py
@@ -23,3 +23,28 @@ for i in range(len(chosen_word)):
     if chosen_word[i] == guess:
         display[i]=guess;
 print(display)
+
+#allowing users to repeat
+import random
+import string
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+print(f'Pssst, the solution is {chosen_word}.')
+display=[]
+for i in chosen_word:
+  display.append("_")
+  a=1
+while(a==1):
+    guess = input("Guess a letter: ").lower()
+    for i in range(len(chosen_word)):
+      if chosen_word[i] == guess:
+        display[i]=guess;
+    print(display)
+    for j in display:
+      if j in string.ascii_letters:
+        a=0
+      else:
+        a=1
+if a==0:
+  print("You win")
+        
