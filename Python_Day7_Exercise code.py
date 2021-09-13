8ef69837b8a7757794fcb43ce79304f4d10da950
GowriMungath <73058923+GowriMungath@users.noreply.github.com>
2021-09-13 17:13:32 +0530

    Update Python_Day7_Exercise code.py

--- Python_Day7_Exercise code.py
+++ Python_Day7_Exercise code.py
@@ -1,1 +1,11 @@
-
+#check if a guessed letter appears in a word
+word_list = ["aardvark", "baboon", "camel"]
+import random as r
+chosen_word=r.choice(word_list)
+print(chosen_word)
+guess=input("Guess a letter ").lower()
+for i in chosen_word:
+  if i == guess:
+    print("right")
+  else:
+    print("wrong")
