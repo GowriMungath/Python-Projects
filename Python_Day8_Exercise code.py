ed2b00e39b67f5a9c24034db6d2754003e105719
GowriMungath <73058923+GowriMungath@users.noreply.github.com>
2021-09-14 16:39:09 +0530

    Update Python_Day8_Exercise code.py

--- Python_Day8_Exercise code.py
+++ Python_Day8_Exercise code.py
@@ -1,1 +1,8 @@
-
+def agepass(my,bro,bf):
+    print(f"My age: {my}")
+    print(f"My brother's age: {bro}")
+    print(f"My boyfriend's age: {bf}")
+    print("\n")
+agepass(20,30,22)
+agepass(30,22,20) #wrong 
+agepass(bro=30,bf=22,my=20)
