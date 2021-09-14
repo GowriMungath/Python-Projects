7115627a7215dfe9f6a7dd40d993e8e6a4738c8c
GowriMungath <73058923+GowriMungath@users.noreply.github.com>
2021-09-14 21:28:44 +0530

    Update Python_Day8_Exercise code.py

--- Python_Day8_Exercise code.py
+++ Python_Day8_Exercise code.py
@@ -32,19 +32,4 @@ n = int(input("Check this number: "))
 prime_checker(number=n)
 
 
-#code1
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-def encrypt(text,shift):
-  new=""
-  for i in range(len(text)):
-    for j in range(len(alphabet)):
-      if text[i]==alphabet[j]:
-       new=new+alphabet[(j+shift)%26]
-  print(f"The encoded text is {new}")
-
-encrypt(text,shift)
 
