1ef0387e45c2b4299afbc5c4b017c64a001fdd4d
GowriMungath <73058923+GowriMungath@users.noreply.github.com>
2021-09-16 10:10:02 +0530

    Update Python_Day10_Exercise code.py

--- Python_Day10_Exercise code.py
+++ Python_Day10_Exercise code.py
@@ -1,1 +1,8 @@
+def name(f_name,l_name):
+    return f_name+" "+l_name
+
+
+f=input("Enter your first name: ").title()
+l=input("Enter your last name: ").title()
+print(f"Your name is: {name(f,l)}")
 
